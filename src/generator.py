31e0dd87dfdc02792389f3c15dbdbad736f759e3
Anand Dyavanapalli <adyavanapalli@gmail.com>
2015-10-06 15:25:23 -0700

    Added documentation.

--- src/generator.py
+++ src/generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""This python module generates n diceware passwords of length l, where l 
-denotes the number of words in the diceware password.  This module assumes that 
-the 'haveged' package is installed on your linux system, as it provides 
+"""This python module generates n diceware passwords of length l, where l
+denotes the number of words in the diceware password.  This module assumes that
+the 'haveged' package is installed on your linux system, as it provides
 excellent entropy.
 """
 
@@ -10,30 +10,31 @@ from os import system
 
 def generate(n, l):
     """This method prints n diceware passwords of length l.
-    
+
     :param n: the number of generated diceware passwords
     :type n: int
     :param l: the length of a diceware password
     :type l: int
-    :returns: prints to the console n diceware passwords of length l 
+    :returns: prints to the console n diceware passwords of length l
     """
     for i in range(n):
-        print("Password {0}:\t{1}".format(i + 1, password(l))) 
+        print("Password {0}:\t{1}".format(i + 1, password(l)))
+
 
 def password(l):
     """This method returns a diceware password string of l words.
-    
-    This method samples cryptographically secure random integers using the 
-    linux coreutils shuf package.  One of the requirements for this module is 
-    that the haveged package is installed as it allows /dev/random's entropy 
+
+    This method samples cryptographically secure random integers using the
+    linux coreutils shuf package.  One of the requirements for this module is
+    that the haveged package is installed as it allows /dev/random's entropy
     pool to never dip below ~1024 bits.
 
     :param l: the length of a diceware password
     :type l: int
     :returns: str representing a diceware password of length l
     """
-    # The {0} is substituted with the number of words in the wordlist (but -1 
-    # because we start counting from 0).  And the {1} is substituted with l, or 
+    # The {0} is substituted with the number of words in the wordlist (but -1
+    # because we start counting from 0).  And the {1} is substituted with l, or
     # the number of words needed in the diceware password.
     system('shuf --input-range=0-{0} --random-source=/dev/random \
             --head-count={1} --output=numbers.txt'.format(LENGTH - 1, l))
